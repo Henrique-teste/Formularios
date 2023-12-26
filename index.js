a2f492cb006e30b61ac14fab1b73db2a465225d3
Henrique-teste <152813884+Henrique-teste@users.noreply.github.com>
2023-12-26 10:02:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-const form = document.getElementById('form-enviar')
-function validaNome (nomeCompleto){
-    const nomeArray = nomeCompleto.split(' ');
-    return nomeArray.length >= 2;
-}
-form.addEventListener('submit',function(e){
-    e.preventDefault()
 
-    
-    const nomeBeneficiario = document.getElementById('nome completo')
-    if (!validaNome(nomeBeneficiario.value)){
-        alert("o nome não está completo")
-    }
-    else{
-        alert("tudo certo")
-        form.reset()
-    }
-})
