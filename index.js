d2db3c48a99596da0adacd86b758ce98fcbe13bf
Henrique-teste <152813884+Henrique-teste@users.noreply.github.com>
2023-12-26 17:52:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const form = document.getElementById('form-enviar')
-form.addEventListerner('submit', function(e){
+form.addEventListener('submit', function(e){
 e.preventDefault();
   console.log('form')
 })
